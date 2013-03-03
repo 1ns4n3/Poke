912c671ce5d4266f4e98738758198b18cf557244
Philippe Creux <pcreux@gmail.com>
2013-03-03 19:15:53 +0100

    Initial commit

---
+++ src/test/java/IUT/BoBot/AppTest.java
@@ -0,0 +1,14 @@
+package IUT.BoBot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
