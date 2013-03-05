85717974ae1437f8634e159fe1672147fcaefe64
Philippe Creux <pcreux@gmail.com>
2013-03-05 13:08:29 +0100

    BonjourWithName is complete

--- src/main/java/IUT/BoBot/SmartCells/BonjourWithNameCell.java
+++ src/main/java/IUT/BoBot/SmartCells/BonjourWithNameCell.java
@@ -11,7 +11,17 @@ public class BonjourWithNameCell implements SmartCell {
 	}
 
 	public String answer() {
-		return "Bonjour Seb!";
+		if (question.contains("m'appelle"))
+			return "Bonjour " + extractName(question) + "!";
+		else
+			return null;
+	}
+	
+	private String extractName(String question) {
+		String bits[] = question.split("appelle ");
+		String name = bits[bits.length - 1];
+		
+		return name;
 	}
 
 }
--- src/test/java/IUT/BoBot/SmartCells/BonjourWithNameCellTest.java
+++ src/test/java/IUT/BoBot/SmartCells/BonjourWithNameCellTest.java
@@ -7,9 +7,20 @@ import org.junit.Test;
 public class BonjourWithNameCellTest {
 
 	@Test
-	public void testBonjourWithName() {
+	public void testBonjourWithNameSeb() {
 		BonjourWithNameCell cell = new BonjourWithNameCell("Je m'appelle Seb");
 		assertEquals("Bonjour Seb!", cell.answer());
 	}
+	
+	@Test
+	public void testBonjourWithNameBob() {
+		BonjourWithNameCell cell = new BonjourWithNameCell("Je m'appelle Bob");
+		assertEquals("Bonjour Bob!", cell.answer());
+	}
 
+	@Test
+	public void testBonjourWithoutName() {
+		BonjourWithNameCell cell = new BonjourWithNameCell("Yo!");
+		assertEquals(null, cell.answer());
+	}
 }
