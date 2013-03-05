795473ac41fa6a6301cabb433efb7719c515840a
Philippe Creux <pcreux@gmail.com>
2013-03-05 15:27:46 +0100

    Add comments

--- src/main/java/IUT/BoBot/Bot.java
+++ src/main/java/IUT/BoBot/Bot.java
@@ -5,13 +5,21 @@ import IUT.BoBot.SmartCells.*;
 
 public class Bot 
 {
+	/**
+	 * List of smartCells the questions are asked to.
+	 */
 	final SmartCell[] smartCells = new SmartCell[] {
 		new BonjourCell(),
 		new BonjourWithNameCell(),
 		new WhosAwesomeCell()
 	};
 
-	
+	/**
+	 * Ask something to BoBot, it will respond to you.
+	 * 
+	 * @param question The question you ask.
+	 * @return An answer... or null if it doesn't get it.
+	 */
 	public String ask(String question) {
 		for(SmartCell cell:smartCells) {
 			String answer = cell.ask(question);
