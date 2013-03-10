e3c9532201b697bd2146f87fbb716b6102492e58
Sébastien NEDJAR <nedseb@goldaire.com>
2013-03-10 23:10:37 +0100

    F-1 première implémentation de la storie

--- src/main/java/fr/univaix/iut/pokebattle/TwitterBot.java
+++ src/main/java/fr/univaix/iut/pokebattle/TwitterBot.java
@@ -1,5 +1,54 @@
 package fr.univaix.iut.pokebattle;
 
+import com.twitter.hbc.httpclient.ControlStreamException;
+import fr.univaix.iut.pokebattle.tuse.TwitterUserStreamEasy;
+import fr.univaix.iut.pokebattle.tuse.UserStreamAdapter;
+import twitter4j.*;
+
+import java.io.IOException;
+
 public class TwitterBot {
 
+    TwitterUserStreamEasy twitterUserStreamEasy;
+
+    public TwitterBot() {
+
+        UserStreamListener listener = new UserStreamAdapter() {
+            PokeBot pokeBot = new PokeBot();
+            Twitter twitter = TwitterFactory.getSingleton();
+
+            @Override
+            public void onStatus(Status status) {
+                try {
+                    System.out.println("TwitterUserStreamEasyExample.onStatus");
+                    String response = pokeBot.ask(status.getText());
+                    System.out.println(response);
+                    if(response != null)
+                        twitter.updateStatus(response);
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        twitterUserStreamEasy = new TwitterUserStreamEasy(listener);
+    }
+
+    public void startBot() throws InterruptedException, ControlStreamException, IOException {
+        twitterUserStreamEasy.oauth();
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            TwitterBot twitterBot = new TwitterBot();
+            twitterBot.startBot();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ControlStreamException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
