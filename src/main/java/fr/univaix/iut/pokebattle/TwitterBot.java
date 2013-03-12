c884bf4f381846fb85430042d7902c62eddc22ae
Sébastien NEDJAR <nedseb@goldaire.com>
2013-03-12 21:23:08 +0100

    F-1 Amélioration du bot twitter

--- src/main/java/fr/univaix/iut/pokebattle/TwitterBot.java
+++ src/main/java/fr/univaix/iut/pokebattle/TwitterBot.java
@@ -3,25 +3,32 @@ package fr.univaix.iut.pokebattle;
 import com.twitter.hbc.httpclient.ControlStreamException;
 import fr.univaix.iut.pokebattle.tuse.TwitterUserStreamEasy;
 import fr.univaix.iut.pokebattle.tuse.UserStreamAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import twitter4j.*;
 
 import java.io.IOException;
 
 public class TwitterBot {
+    private final static Logger logger = LoggerFactory.getLogger(TwitterBot.class);
 
     TwitterUserStreamEasy twitterUserStreamEasy;
+    Twitter twitter = TwitterFactory.getSingleton();
 
     public TwitterBot(final Bot bot) {
         UserStreamListener listener = new UserStreamAdapter() {
 
-            Twitter twitter = TwitterFactory.getSingleton();
 
             @Override
             public void onStatus(Status status) {
                 try {
-                    System.out.println("TwitterUserStreamEasyExample.onStatus");
+                    logger.info("TwitterUserStreamEasyExample.onStatus()");
+                    if (isTweetOfMe(status) || !isTweetForMe(status)) {
+                        logger.info("Ignored status change");
+                        return;
+                    }
+
                     String response = bot.ask(status.getText());
-                    System.out.println(response);
                     if (response != null)
                         twitter.updateStatus(response);
                 } catch (TwitterException e) {
@@ -33,8 +40,16 @@ public class TwitterBot {
         twitterUserStreamEasy = new TwitterUserStreamEasy(listener);
     }
 
+    private boolean isTweetForMe(Status status) throws TwitterException {
+        return status.getText().contains(twitter.getScreenName());
+    }
+
     public void startBot() throws InterruptedException, ControlStreamException, IOException {
         twitterUserStreamEasy.oauth();
+    }
+
+    private boolean isTweetOfMe(Status status) throws TwitterException {
+        return status.getUser().getId() == twitter.getId();
     }
 
     public static void main(String[] args) {
